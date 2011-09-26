2b1cc643e37a98e1cea98a5fadff2e431d8604fb
Joao Silva <kappy@acydburne.com.pt>
2011-09-27 00:34:08 +0100

    Added the service container

---
+++ ShowBot-WindowsService/Service1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using Ninject;
+using ShowBot.Infrastructure;
+using System.Timers;
+
+namespace ShowBot_WindowsService {
+	public partial class Service1 : ServiceBase {
+		private readonly IKernel kernel;
+		private readonly IEngine engine;
+		private readonly Timer timer;
+		private DateTime lastTimeChecked;
+
+		public Service1() {
+			InitializeComponent();
+			kernel = new StandardKernel(new DefaultNinjectModule());
+			engine = kernel.Get<IEngine>();
+			timer = new Timer(60000);
+			timer.Enabled = false;
+			timer.Elapsed += new ElapsedEventHandler(timer_Elapsed);
+			this.lastTimeChecked = DateTime.Now;
+		}
+
+		void timer_Elapsed(object sender, ElapsedEventArgs e) {
+			var dateNow = DateTime.Now;
+			engine.CheckForNewShows(lastTimeChecked);
+			lastTimeChecked = dateNow;
+			engine.CheckStatus();
+		}
+
+		protected override void OnStart(string[] args) {
+			timer.Enabled = true;
+		}
+
+		protected override void OnStop() {
+			timer.Enabled = false;
+		}
+	}
+}
