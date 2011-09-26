0685f1485301311bb8cf98bc24f90f2925413f8d
Joao Silva <kappy@acydburne.com.pt>
2011-09-27 00:10:56 +0100

    Added DI first draft

---
+++ ShowBot/Infrastructure/DefaultNinjectModule.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ninject;
+using Ninject.Modules;
+using ShowBot.Services;
+using ShowBot.DefaultServices;
+
+namespace ShowBot.Infrastructure {
+	public class DefaultNinjectModule : NinjectModule {
+		public override void Load() {
+			BindDownloader();
+			BindNewShowsProvider();
+			BindNotifier();
+			BindSubtitler();
+			Kernel.Bind<IEngine>().To<Engine>();
+		}
+
+		public virtual void BindNotifier(){
+			Kernel.Bind<INotifier>().To<EMailNotifier>();
+		}
+
+		public virtual void BindSubtitler() {
+			Kernel.Bind<ISubtitler>().To<OSDBNetSubtitler>();
+		}
+
+		public virtual void BindDownloader() {
+			Kernel.Bind<IDownloader>().To<TransmissionNetDownloader>();
+		}
+
+		public virtual void BindNewShowsProvider(){
+			Kernel.Bind<INewShowsProvider>().To<ShowRssNewShowProvider>();
+		}
+	}
+}
