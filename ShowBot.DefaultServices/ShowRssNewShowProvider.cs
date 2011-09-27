a1a2ff186f9a04983e47770cf97957efe1113bae
Joao Silva <kappy@acydburne.com.pt>
2011-09-27 11:49:01 +0100

    Implemented the ShowRssNewShowsProvider

--- ShowBot.DefaultServices/ShowRssNewShowProvider.cs
+++ ShowBot.DefaultServices/ShowRssNewShowProvider.cs
@@ -4,11 +4,32 @@ using System.Linq;
 using System.Text;
 using ShowBot.Services;
 using ShowBot.Model;
+using Argotic.Syndication;
 
 namespace ShowBot.DefaultServices {
 	public class ShowRssNewShowProvider : INewShowsProvider {
+
+		private readonly string feedUrl;
+
+		public ShowRssNewShowProvider(IConfig config) {
+			var settings = config.GetConfigurationSettings();
+			feedUrl = settings.FeedUrl;
+		}
+
 		public IEnumerable<Show> GetNewShowsSince(DateTime sinceDate) {
-			return new List<Show>();
+
+			RssFeed feed = RssFeed.Create(
+				new Uri(feedUrl), 
+				new Argotic.Common.SyndicationResourceLoadSettings {
+					RetrievalLimit = 10,
+					AutoDetectExtensions = true,
+			});
+
+			var newShows = from item in feed.Channel.Items
+						   where item.PublicationDate >= sinceDate
+						   select new Show { Title = item.Title, TorrentFile = item.Link.ToString() };
+
+			return newShows;
 		}
 	}
 }
