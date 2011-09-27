da6fd956eb3ed3ee395d9ec369270e29d90778e9
Joao Silva <kappy@acydburne.com.pt>
2011-09-27 11:35:10 +0100

    Implemented the Email Notifier

--- ShowBot.DefaultServices/EMailNotifier.cs
+++ ShowBot.DefaultServices/EMailNotifier.cs
@@ -4,11 +4,35 @@ using System.Linq;
 using System.Text;
 using ShowBot.Services;
 using ShowBot.Model;
+using System.Net.Mail;
+using System.Net;
 
 namespace ShowBot.DefaultServices {
 	public class EMailNotifier : INotifier {
 
+		dynamic settings;
+		private readonly string smtpServer;
+		private readonly string smtpUser;
+		private readonly string smtpPassword;
+		private readonly string from;
+		private readonly string to;
+		private readonly string subject;
+
+		public EMailNotifier(IConfig config) {
+			settings = config.GetConfigurationSettings();
+			smtpServer = settings.SMTPServer;
+			smtpUser = settings.SMTPUser;
+			smtpPassword = settings.SMTPPassword;
+			from = settings.From;
+			to = settings.To;
+			subject = settings.Subject;
+		}
+
 		public void Notify(string message) {
+			var mailMessage = new MailMessage(from, to, subject, message);
+			var smtp = new SmtpClient(smtpServer);
+			smtp.Credentials = new NetworkCredential(smtpUser, smtpPassword);
+			smtp.Send(mailMessage);
 		}
 	}
 }
