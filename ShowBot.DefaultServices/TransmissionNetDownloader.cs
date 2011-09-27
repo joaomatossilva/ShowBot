b23cbdb630fbaef312accf412f033c0336ee626c
Joao Silva <kappy@acydburne.com.pt>
2011-09-27 17:26:27 +0100

    Fixed some transmissions on windows issues on base download dir

--- ShowBot.DefaultServices/TransmissionNetDownloader.cs
+++ ShowBot.DefaultServices/TransmissionNetDownloader.cs
@@ -5,17 +5,20 @@ using System.Text;
 using ShowBot.Services;
 using ShowBot.Model;
 using TransmissionNet;
+using System.IO;
 
 namespace ShowBot.DefaultServices {
 	public class TransmissionNetDownloader : IDownloader {
 
 		private readonly ITransmission transmission;
+		private readonly string baseDownloadDir;
 
 
 		public TransmissionNetDownloader(IConfig config) {
 			var settings = config.GetConfigurationSettings();
 			string url = settings.TransmissionUrl;
 			transmission = new TransmissionRPC(new Uri(url));
+			baseDownloadDir = settings.BaseDownloadDir;
 		}
 
 		public Download AddDownload(Show showToDownload) {
@@ -36,9 +39,9 @@ namespace ShowBot.DefaultServices {
 										select file.name).ToList()
 									 ) {
 										 Id = torrent.id,
-										 Path = torrent.downloadDir,
+										 Path = HandleDownloadDir(torrent.downloadDir),
 										 Progress = torrent.percentDone,
-										 Status = torrent.isFinished ? DownloadStatus.Finished : DownloadStatus.InProgress,
+										 Status = torrent.percentDone == 1.0 ? DownloadStatus.Finished : DownloadStatus.InProgress,
 										 TorrentFile = torrent.torrentfile
 								 };
 			return statusTorrents;
@@ -46,6 +49,13 @@ namespace ShowBot.DefaultServices {
 
 		public void RemoveDownload(Download downloadToRemove) {
 			transmission.RemoveTorrent(downloadToRemove.Id);
+		}
+
+		private string HandleDownloadDir(string torrentDownloadDir) {
+			if(string.IsNullOrEmpty(baseDownloadDir)){
+				return torrentDownloadDir;
+			}
+			return Path.Combine(baseDownloadDir, Path.IsPathRooted(torrentDownloadDir) ? torrentDownloadDir.Substring(1) : torrentDownloadDir);
 		}
 	}
 }
