7a46c9aeda8933bcc321e292f4a9718af479f70e
Joao Silva <kappy@acydburne.com.pt>
2011-09-27 23:59:52 +0100

    Added persitense of list datetime checked for shows

--- ShowBot-WindowsService/Service1.cs
+++ ShowBot-WindowsService/Service1.cs
@@ -9,29 +9,53 @@ using System.Text;
 using Ninject;
 using ShowBot.Infrastructure;
 using System.Timers;
+using System.Configuration;
 
 namespace ShowBot_WindowsService {
 	public partial class Service1 : ServiceBase {
+
+		private const string INTERVAL_KEY = "Interval";
+		private const string LAST_CHECK_DATE_KEY = "LastCheckDate";
+
 		private readonly IKernel kernel;
 		private readonly IEngine engine;
 		private readonly Timer timer;
+		private readonly double interval;
 		private DateTime lastTimeChecked;
 
 		public Service1() {
 			InitializeComponent();
+
+			var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+			interval = double.Parse(config.AppSettings.Settings[INTERVAL_KEY].Value);
+			var lastTimeChecketString = config.AppSettings.Settings[LAST_CHECK_DATE_KEY].Value;
+			if (string.IsNullOrEmpty(lastTimeChecketString)) {
+				lastTimeChecked = DateTime.Now.AddMilliseconds(-1 * interval);
+				config.AppSettings.Settings[LAST_CHECK_DATE_KEY].Value = lastTimeChecked.ToString();
+				config.Save();
+			} else {
+				lastTimeChecked = DateTime.Parse(lastTimeChecketString);
+			}			
+
 			kernel = new StandardKernel(new DefaultNinjectModule());
 			engine = kernel.Get<IEngine>();
-			timer = new Timer(60000);
+			timer = new Timer(interval);
 			timer.Enabled = false;
 			timer.Elapsed += new ElapsedEventHandler(timer_Elapsed);
-			this.lastTimeChecked = DateTime.Now;
 		}
 
 		void timer_Elapsed(object sender, ElapsedEventArgs e) {
 			var dateNow = DateTime.Now;
 			engine.CheckForNewShows(lastTimeChecked);
 			lastTimeChecked = dateNow;
+			PersistLastTimeCheck(lastTimeChecked);
 			engine.CheckStatus();
+		}
+
+		private void PersistLastTimeCheck(DateTime lastTimeChecked) {
+			var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+			config.AppSettings.Settings[LAST_CHECK_DATE_KEY].Value = lastTimeChecked.ToString();
+			config.Save();
 		}
 
 		protected override void OnStart(string[] args) {
