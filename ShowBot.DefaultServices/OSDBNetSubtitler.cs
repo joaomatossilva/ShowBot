52f287d5f119e854445c277fa3f18c7d27c9ae73
Joao Silva <kappy@acydburne.com.pt>
2011-09-27 16:16:04 +0100

    Added subtitler implementation

--- ShowBot.DefaultServices/OSDBNetSubtitler.cs
+++ ShowBot.DefaultServices/OSDBNetSubtitler.cs
@@ -4,11 +4,28 @@ using System.Linq;
 using System.Text;
 using ShowBot.Services;
 using ShowBot.Model;
+using OSDBnet;
+using System.IO;
 
 namespace ShowBot.DefaultServices {
 	public class OSDBNetSubtitler : ISubtitler {
+
+		private readonly string userAgent;
+
+		public OSDBNetSubtitler(IConfig config) {
+			var settings = config.GetConfigurationSettings();
+			userAgent = settings.UserAgent;
+		}
+
 		public bool GetSubtitleForFile(string movieFilePath) {
-			return false;
+			using (IAnonymousClient client = Osdb.Login("", userAgent)) {
+				var subtitlesFound = client.SearchSubtitles(movieFilePath);
+				if (subtitlesFound.Count == 0) {
+					return false;
+				}
+				client.DownloadSubtitleToPath(Path.GetDirectoryName(movieFilePath), subtitlesFound.First());
+			}
+			return true;
 		}
 	}
 }
