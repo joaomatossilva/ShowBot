5d23ca0cda3d3a09337842c6dd96c581d1f325e3
Joao Silva <kappy@acydburne.com.pt>
2011-09-28 17:09:16 +0100

    Added a service Wrapper to Mono since wasn't been able to start the Windows Service Project with mono-service2

---
+++ ShowBot-MonoDaemon/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mono.Unix;
+using Mono.Unix.Native;
+
+namespace ShowBot_MonoDaemon {
+	class Program {
+		static void Main(string[] args) {
+			UnixSignal intr = new UnixSignal (Signum.SIGINT);
+			UnixSignal term = new UnixSignal (Signum.SIGTERM);
+			UnixSignal hup = new UnixSignal (Signum.SIGHUP);
+			UnixSignal usr2 = new UnixSignal (Signum.SIGUSR2);
+
+			UnixSignal[] signals = new UnixSignal[] { intr, term, hup, usr2 };
+
+			Console.WriteLine("daemon: engine starting...");
+
+			var service = new Service();
+			service.Start(null);
+
+			Console.WriteLine("daemon: engine started...");
+
+
+			for (bool running = true; running; )
+			{
+				int idx = UnixSignal.WaitAny(signals);
+
+				if (idx < 0 || idx >= signals.Length) continue;
+
+				Console.WriteLine(string.Format("daemon: received signal {0}",signals[idx].Signum));
+
+				if ((intr.IsSet || term.IsSet)) 
+				{
+					intr.Reset ();
+					term.Reset ();
+
+					Console.WriteLine("daemon: stopping...");
+
+					running = false;
+				}
+				else if (hup.IsSet)
+				{
+					// Ignore. Could be used to reload configuration.
+					hup.Reset();
+				}
+				else if (usr2.IsSet)
+				{
+					usr2.Reset();
+					// do something
+				}
+			}
+
+			service.Stop();
+		}
+	}
+}
---
+++ ShowBot-MonoDaemon/Service.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+using System.Configuration;
+using Ninject;
+using ShowBot.Infrastructure;
+using ShowBot.DefaultServices;
+using ShowBot.Services;
+using ShowBot;
+
+namespace ShowBot_MonoDaemon {
+	public class Service {
+		private const string INTERVAL_KEY = "Interval";
+		private const string LAST_CHECK_DATE_KEY = "LastCheckDate";
+
+		private readonly IKernel kernel;
+		private readonly IEngine engine;
+		private readonly Timer timer;
+		private readonly double interval;
+		private DateTime lastTimeChecked;
+
+		public Service() {
+
+			var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+			interval = double.Parse(config.AppSettings.Settings[INTERVAL_KEY].Value);
+			var lastTimeChecketString = config.AppSettings.Settings[LAST_CHECK_DATE_KEY].Value;
+			if (string.IsNullOrEmpty(lastTimeChecketString)) {
+				lastTimeChecked = DateTime.Now.AddMilliseconds(-1 * interval);
+				config.AppSettings.Settings[LAST_CHECK_DATE_KEY].Value = lastTimeChecked.ToString();
+				config.Save();
+				Console.WriteLine("last check date saved");
+			} else {
+				lastTimeChecked = DateTime.Parse(lastTimeChecketString);
+			}			
+			/*
+			kernel = new StandardKernel(new DefaultNinjectModule());
+			engine = kernel.Get<IEngine>();*/
+			
+			///************** Temp modification since there is a bug on Ninject
+			kernel = new StandardKernel(new DefaultNinjectModule());
+
+			INotifier notifier = new EMailNotifier(new DynamicConfigReader("EMailNotifier"));
+			ISubtitler subtitler = new OSDBNetSubtitler(new DynamicConfigReader("OSDBNetSubtitler"));
+			IDownloader downloader = new TransmissionNetDownloader(new DynamicConfigReader("TransmissionNetDownloader"));
+			INewShowsProvider showsProvider = new ShowRssNewShowProvider(new DynamicConfigReader("ShowRssNewShowProvider"));
+			engine = new Engine(downloader, showsProvider, subtitler, notifier);
+			///************** End Temp modification 
+
+
+			timer = new Timer(interval);
+			timer.Enabled = false;
+			timer.Elapsed += new ElapsedEventHandler(timer_Elapsed);
+		}
+
+		void timer_Elapsed(object sender, ElapsedEventArgs e) {
+			Console.WriteLine("timer tick");
+			var dateNow = DateTime.Now;
+			engine.CheckForNewShows(lastTimeChecked);
+			lastTimeChecked = dateNow;
+			PersistLastTimeCheck(lastTimeChecked);
+			engine.CheckStatus();
+		}
+
+		private void PersistLastTimeCheck(DateTime lastTimeChecked) {
+			var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+			config.AppSettings.Settings[LAST_CHECK_DATE_KEY].Value = lastTimeChecked.ToString();
+			config.Save();
+			Console.WriteLine("last check date saved");
+		}
+
+		public void Start(string[] args) {
+			Console.WriteLine("starting service");
+			timer.Enabled = true;
+		}
+
+		public void Stop() {
+			Console.WriteLine("stopping service");
+			timer.Enabled = false;
+		}
+	}
+}
