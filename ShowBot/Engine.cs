d804f492858b3c5f80e9b49127dbf2a5b6100b37
Joao Silva <kappy@acydburne.com.pt>
2011-09-29 14:00:49 +0100

    Added some debuging to console

--- ShowBot/Engine.cs
+++ ShowBot/Engine.cs
@@ -23,17 +23,24 @@ namespace ShowBot {
 		}
 
 		public void CheckForNewShows(DateTime lastExecutionDate) {
+			Console.WriteLine("Checking for new shows...");
 			var newShows = newShowsProvider.GetNewShowsSince(lastExecutionDate);
+			Console.WriteLine("{0} new shows found.", newShows.Count());
 
 			foreach (var newShow in newShows) {
-				downloader.AddDownload(newShow);
+				Console.WriteLine("Downloading show {0} : {1}", newShow.Title, newShow.TorrentFile);
+				var download = downloader.AddDownload(newShow);
+				Console.WriteLine("Show is beeing downloaded with id {0}", download.Id);
 			}
 		}
 
 		public void CheckStatus() {
+			Console.WriteLine("Checking for download status");
 			var currentDownloads = downloader.GetStatus();
+			Console.WriteLine("{0} currently active downloads", currentDownloads.Count());
 
 			foreach (var download in currentDownloads) {
+				Console.WriteLine("{0} is at {1} - {2}", download.Id, download.Progress, download.Status.ToString());
 				if (download.Status == Model.DownloadStatus.Finished) {
 					HandleFinishedDownload(download);
 				}
@@ -42,11 +49,16 @@ namespace ShowBot {
 
 		private void HandleFinishedDownload(Download finishedDownload) {
 			try {
+				Console.WriteLine("Handling finished download id {0}...", finishedDownload.Id);
 				string movieFile = GuessMovieFile(finishedDownload.Path, finishedDownload.Files);
 				string movieFullPath = Path.Combine(finishedDownload.Path, movieFile);
+				Console.WriteLine("Getting subtitles for movie {0}", movieFullPath);
 				bool couldFindSubtitle = subtitler.GetSubtitleForFile(movieFullPath);
+				Console.WriteLine("Subtitles found? {0}", couldFindSubtitle);
 				if (couldFindSubtitle) {
+					Console.WriteLine("Removing finished download...");
 					downloader.RemoveDownload(finishedDownload);
+					Console.WriteLine("finished download removed.");
 					notifier.Notify(String.Format("The movie {0} is completed", movieFile));
 				}
 			} catch (Exception ex) {
