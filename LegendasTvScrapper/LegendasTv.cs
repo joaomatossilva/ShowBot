9c9a63155f8a6cc628e4f0acd34fac9023a169e7
Joao Silva <kappy@acydburne.com.pt>
2011-11-29 15:18:48 +0000

    Added LegedendasTv Website Scrapper subtitler module.

---
+++ LegendasTvScrapper/LegendasTv.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Xml.Linq;
+using HtmlAgilityPack;
+
+namespace LegendasTvScrapper {
+	public class LegendasTv : IDisposable{
+		private const string SiteUrl = "http://legendas.tv";
+		private bool disposed;
+		private CookieCollection cookies;
+
+		public void Login(string userName, string password) {
+			MakePost(SiteUrl + "/login_verificar.php", string.Format("txtLogin={0}&txtSenha={1}&entrar.x=18&entrar.y=14", userName, password));
+		}
+
+		public IList<Legenda> Search(string search) {
+			const string searchDataFormat = "txtLegenda={0}&selTipo=1&int_idioma=1&btn_buscar.x=28&btn_buscar.y=7";
+			var searchData = string.Format(searchDataFormat, HttpUtility.HtmlEncode(search).Replace(" ", "+"));
+			string response = MakePost(SiteUrl + "/index.php?opcao=buscarlegenda", searchData);
+
+			return ParseHtml(response);			
+		}
+
+
+		private IList<Legenda> ParseHtml(string html) {
+			const string startIdString = "javascript:abredown('";
+			const int idLength = 32;
+			var doc = new HtmlDocument();
+			doc.LoadHtml(html);
+			var legendaTables = doc.DocumentNode.SelectNodes("//table[@class=\"buscaNDestaque\" or @class=\"buscaDestaque\"]");
+
+			var legendas = new List<Legenda>();
+			if (legendaTables == null) {
+				return legendas;
+			}
+			foreach (var legendaTable in legendaTables) {
+				var id = legendaTable.Attributes["onclick"].Value.Replace(startIdString, "").Substring(0, idLength);
+				var nameNome = legendaTable.SelectSingleNode("./tr/td/span[@class=\"brls\"]");
+				var name = nameNome.InnerText;
+				legendas.Add(new Legenda(id, name));
+			}
+			return legendas;
+		}
+
+		public string DownloadLegenda(string id) {
+			var request = (HttpWebRequest) WebRequest.Create(string.Format(SiteUrl + "/info.php?d={0}&c=1", id));
+			request.CookieContainer = new CookieContainer();
+			if (cookies != null) {
+				request.CookieContainer.Add(cookies);
+			}
+			var response = (HttpWebResponse) request.GetResponse();
+			using (var responseStream = response.GetResponseStream()) {
+				var localFile = Path.GetTempFileName();
+				using (var localStream = File.Create(localFile)) {
+					byte[] buffer = new byte[1024];
+					int bytesRead;
+					do {
+						bytesRead = responseStream.Read(buffer, 0, buffer.Length);
+						localStream.Write(buffer, 0, bytesRead);
+					} while (bytesRead > 0);
+					return localFile;
+				}
+			}
+		}
+
+		private string MakePost(string url, string postData) {
+			var request = (HttpWebRequest)WebRequest.Create(url);
+			request.CookieContainer = new CookieContainer();
+			request.Method = "POST";
+			request.ContentType = "application/x-www-form-urlencoded";
+			byte[] bytes = Encoding.UTF8.GetBytes(postData);
+			request.ContentLength = bytes.Length;
+			if (cookies != null) {
+				request.CookieContainer.Add(cookies);
+			}
+
+			Stream requestStream = request.GetRequestStream();
+			requestStream.Write(bytes, 0, bytes.Length);
+
+			var response = (HttpWebResponse)request.GetResponse();
+			if (response.Cookies != null && response.Cookies.Count > 0)
+				this.cookies = response.Cookies;
+			using (var stream = response.GetResponseStream()) {
+				using (var reader = new StreamReader(stream)) {
+					return reader.ReadToEnd();
+				}
+			}
+		}
+
+		public void Dispose() {
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		private void Dispose(bool disposing) {
+			if (!disposed) {
+				if (disposing) {
+				}
+				disposed = true;
+			}
+		}
+
+		~LegendasTv() {
+			Dispose(false);
+		}
+	}
+}
---
+++ ShowBot.DefaultServices/LegendasTvSubtitler.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using LegendasTvScrapper;
+using SharpCompress.Common;
+using SharpCompress.Reader;
+using ShowBot.Services;
+
+namespace ShowBot.DefaultServices {
+	public class LegendasTvSubtitler : ISubtitler{
+		private string userName;
+		private string password;
+
+		public LegendasTvSubtitler(IConfig config) {
+			var settings = config.GetConfigurationSettings();
+			userName = settings.UserName;
+			password = settings.Password;
+		}
+
+		public bool GetSubtitleForFile(string movieFilePath) {
+			using (var legendasTv = new LegendasTv()) {
+				legendasTv.Login(userName, password);
+				string fileName = Path.GetFileNameWithoutExtension(movieFilePath);
+				var normalizedFileName = NormalizeSearchString(fileName);
+				var legendas = legendasTv.Search(normalizedFileName);
+				if (legendas.Count == 0) {
+					return false;
+				}
+				string subtitlesZipFile = legendasTv.DownloadLegenda(legendas.First().Id);
+				try {
+					var tempDiretory = CreateTempDirectory();
+					try {
+						ExctractFilesFromArchive(subtitlesZipFile, tempDiretory);
+
+						var entriesList = (from string file in Directory.GetFiles(tempDiretory) select Path.GetFileName(file)).ToList();
+						var bestMatch = GetBestMatch(fileName, entriesList);
+						if (string.IsNullOrEmpty(bestMatch)) {
+							return false;
+						}
+
+						var destinationSubtitleFile = Path.Combine(Path.GetDirectoryName(movieFilePath), fileName + Path.GetExtension(bestMatch));
+						if (File.Exists(destinationSubtitleFile)) {
+							File.Delete(destinationSubtitleFile);
+						}
+						File.Move(Path.Combine(tempDiretory, bestMatch), destinationSubtitleFile);
+
+						return true;
+					} finally {
+						Directory.Delete(tempDiretory, true);
+					}
+				} finally {
+					File.Delete(subtitlesZipFile);
+				}
+			}
+		}
+
+		private static void ExctractFilesFromArchive(string archiveFile, string extractToPath) {
+			using (var zipFileStream = new FileStream(archiveFile, FileMode.Open, FileAccess.Read)) {
+				var reader = ReaderFactory.Open(zipFileStream);
+				while (reader.MoveToNextEntry()) {
+					if (!reader.Entry.IsDirectory) {
+						Console.WriteLine(reader.Entry.FilePath);
+						reader.WriteEntryToDirectory(extractToPath, ExtractOptions.ExtractFullPath | ExtractOptions.Overwrite);
+					}
+				}
+			}
+		}
+
+		private static string CreateTempDirectory() {
+			var tempFile = Path.GetTempFileName();
+			File.Delete(tempFile);
+			var tempDirectoryPath = Path.Combine(Path.GetDirectoryName(tempFile), Path.GetFileNameWithoutExtension(tempFile));
+			Directory.CreateDirectory(tempDirectoryPath);
+			return tempDirectoryPath;
+		}
+
+		private static string GetBestMatch(string needle, IEnumerable<string> haystack) {
+			var delimiters = new char[] {'.', '-', ' '};
+			var needleParts = needle.Split(delimiters);
+			var bestMatchRatioSoFar = 0;
+			var bestMatchSubjectSoFar = string.Empty;
+			var partsCount = needleParts.Length;
+			foreach (string subject in haystack) {
+				int matches = 0;
+				foreach (string needlePart in needleParts) {
+					if (subject.Contains(needlePart)) {
+						matches++;
+					}
+				}
+				var matchRatio = (matches*100)/partsCount;
+				if (matchRatio > bestMatchRatioSoFar) {
+					bestMatchRatioSoFar = matchRatio;
+					bestMatchSubjectSoFar = subject;
+				}
+			}
+			return bestMatchSubjectSoFar;
+		}
+
+		private static readonly Regex showNameEpisodeAndSeasonRegularExpression = new Regex(@"(.*?)\.S?(\d{1,2})E?(\d{2})\.(.*)", RegexOptions.IgnoreCase);
+
+		private static string NormalizeSearchString(string movieFileName) {
+			var matches = showNameEpisodeAndSeasonRegularExpression.Match(movieFileName);
+			if (matches.Success) {
+				return matches.Groups[1].ToString().Trim() + " S" + matches.Groups[2].ToString() + "E" + matches.Groups[3];
+			}
+			return movieFileName;
+		}
+	}
+}
